eb0a796a0f35a1910c640e09e302160a581d2cfe
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-10-10 06:52:14 +0200

    Varus menu setup

--- ElVarusRevamped/ElVarusRevamped/Components/MyMenu.cs
+++ ElVarusRevamped/ElVarusRevamped/Components/MyMenu.cs
@@ -1,12 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElVarusRevamped.Components
+﻿namespace ElVarusRevamped.Components
 {
-    class MyMenu
+    using System;
+
+    using ElVarusRevamped.Enumerations;
+    using ElVarusRevamped.Utils;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    /// <summary>
+    ///     The my menu class.
+    /// </summary>
+    internal class MyMenu
     {
+        #region Constructors and Destructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MyMenu" /> class.
+        /// </summary>
+        internal MyMenu()
+        {
+            RootMenu = new Menu("ElVarusRevamped", "elvarusrevamped", true);
+
+            RootMenu.AddSubMenu(GetTargetSelectorNode());
+            RootMenu.AddSubMenu(GetOrbwalkerNode());
+
+            RootMenu.AddToMainMenu();
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the root menu.
+        /// </summary>
+        internal static Menu RootMenu { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     The generate spell menu method.
+        /// </summary>
+        /// <param name="spellSlot">
+        ///     The spell slot.
+        /// </param>
+        internal static void GenerateSpellMenu(SpellSlot spellSlot)
+        {
+            try
+            {
+                var spellSlotName = spellSlot.ToString();
+                var spellSlotNameLower = spellSlotName.ToLower();
+
+                var node = new Menu(spellSlot + " Settings", "spellmenu" + spellSlotNameLower);
+
+                var nodeCombo = new Menu("Combo", spellSlotNameLower + "combomenu");
+                {
+                    nodeCombo.AddItem(new MenuItem("combo" + spellSlotNameLower + "use", "Use " + spellSlotName).SetValue(true));
+                    nodeCombo.AddItem(new MenuItem("combo" + spellSlotNameLower + "mana", "Min. Mana").SetValue(new Slider(5)));
+                }
+
+                node.AddSubMenu(nodeCombo);
+
+                var nodeMixed = new Menu("Mixed", spellSlotNameLower + "mixedmenu");
+                {
+                    nodeMixed.AddItem(new MenuItem("mixed" + spellSlotNameLower + "use", "Use " + spellSlotName).SetValue(true));
+                    nodeMixed.AddItem(new MenuItem("mixed" + spellSlotNameLower + "mana", "Min. Mana").SetValue(new Slider(50)));
+                }
+
+                node.AddSubMenu(nodeMixed);
+
+                if (spellSlotNameLower.Equals("q", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var nodeLastHit = new Menu("LastHit", spellSlotNameLower + "lasthitmenu");
+                    {
+                        nodeLastHit.AddItem(
+                            new MenuItem("lasthit" + spellSlotNameLower + "use", "Use " + spellSlotName).SetValue(true));
+                        nodeLastHit.AddItem(
+                            new MenuItem("lasthit" + spellSlotNameLower + "mana", "Min. Mana").SetValue(new Slider(50)));
+                    }
+                    node.AddSubMenu(nodeLastHit);
+                }
+
+
+                if (!spellSlotNameLower.Equals("r", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var nodeLaneClear = new Menu("Clear", spellSlotNameLower + "laneclearmenu");
+                    {
+                        nodeLaneClear.AddItem(new MenuItem("laneclear" + spellSlotNameLower + "use", "Use " + spellSlotName).SetValue(true));
+                        nodeLaneClear.AddItem(new MenuItem("laneclear" + spellSlotNameLower + "mana", "Min. Mana").SetValue(new Slider(50)));
+
+                        // Creeps hit slider
+                        if (spellSlotNameLower.Equals("q", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            nodeLaneClear.AddItem(new MenuItem("lasthit.count", "Minions hit Q").SetValue(new Slider(3, 1, 6)));
+                        }
+                    }
+
+                    node.AddSubMenu(nodeLaneClear);
+                }
+
+                RootMenu.AddSubMenu(node);
+            }
+            catch (Exception e)
+            {
+                Logging.AddEntry(LoggingEntryTrype.Error, "@MyMenu.cs: Can not generate menu for spell - {0}", e);
+                throw;
+            }
+        }
+
+        /// <summary>
+        ///     The get orbwalker node.
+        /// </summary>
+        /// <returns>
+        ///     <see cref="Menu" /> class for the orbwalker.
+        /// </returns>
+        private static Menu GetOrbwalkerNode()
+        {
+            var node = new Menu("Orbwalker", "elvarusrevampedorbwalker");
+            Program.Orbwalker = new Orbwalking.Orbwalker(node);
+
+            return node;
+        }
+
+        /// <summary>
+        ///     The get target selector node.
+        /// </summary>
+        /// <returns>
+        ///     <see cref="Menu" /> class for the target selector.
+        /// </returns>
+        private static Menu GetTargetSelectorNode()
+        {
+            var node = new Menu("Target Selector", "elvarusrevampedtargetselector");
+            TargetSelector.AddToMenu(node);
+
+            return node;
+        }
+
+        #endregion
     }
 }
--- ElVarusRevamped/ElVarusRevamped/Program.cs
+++ ElVarusRevamped/ElVarusRevamped/Program.cs
@@ -1,15 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElVarusRevamped
+﻿namespace ElVarusRevamped
 {
-    class Program
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using ElVarusRevamped.Components;
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    /// <summary>
+    ///     The program.
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the orbwalker.
+        /// </summary>
+        internal static Orbwalking.Orbwalker Orbwalker { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     The EntryPoint of the solution.
+        /// </summary>
+        /// <param name="args">
+        ///     The args.
+        /// </param>
+        public static void Main(string[] args)
         {
+            CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                if (ObjectManager.Player.ChampionName.Equals("Varus"))
+                {
+                    Bootstrap();
+                }
+            };
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     The bootstrapping method for the components.
+        /// </summary>
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement", Justification = "They would not be used.")]
+        private static void Bootstrap()
+        {
+            try
+            {
+                new MyMenu();
+                new SpellManager();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("@Program.cs: Can not bootstrap components - {0}", e);
+                throw;
+            }
+        }
+
+        #endregion
     }
 }

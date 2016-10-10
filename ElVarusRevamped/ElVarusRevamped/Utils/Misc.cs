d9152afa13ce828d09f6a8f026f509e808b7e580
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-10-10 06:14:13 +0200

    Varus project base

---
+++ ElVarusRevamped/ElVarusRevamped/Utils/Misc.cs
@@ -0,0 +1,45 @@
+﻿namespace ElVarusRevamped.Utils
+{
+    using System;
+    using System.Linq;
+
+    using ElVarusRevamped.Enumerations;
+
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
+    /// <summary>
+    ///     The misc.
+    /// </summary>
+    internal static class Misc
+    {
+        #region Methods
+
+        /// <summary>
+        ///     Gets a target from the common target selector.
+        /// </summary>
+        /// <param name="range">
+        ///     The range.
+        /// </param>
+        /// <param name="damageType">
+        ///     The damage type.
+        /// </param>
+        /// <returns>
+        ///     <see cref="Obj_AI_Hero" />
+        /// </returns>
+        internal static Obj_AI_Hero GetTarget(float range, TargetSelector.DamageType damageType)
+        {
+            try
+            {
+                return TargetSelector.GetTarget(range, damageType);
+            }
+            catch (Exception e)
+            {
+                Logging.AddEntry(LoggingEntryTrype.Error, "@Misc.cs: Can not return target - {0}", e);
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
